d8aad6fb3906d31f19dfd6c27350c2a4b0fc35bf
max6811 <maxc.m6811@gmail.com>
2018-03-22 09:45:17 -0400

    local storage1

--- src/app/plantilla/appmenu/appmenu.component.ts
+++ src/app/plantilla/appmenu/appmenu.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appmenu.component.css']
 })
 export class AppmenuComponent implements OnInit {
-
+  public nombreUser:string;
   constructor() { }
 
   ngOnInit() {
+    //obteniendo sesion de usuario del localStorage
+    this.nombreUser = localStorage.getItem('nombreUser');
   }
 
 }
